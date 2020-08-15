b3e192e23636f0cfec0141f6b4c467576a382524
Matthew Jagiela <>
2020-08-15 12:58:21 -0400

    Live data

--- uApps-Support/uApps-Support/SwiftUI/FeedbackTableView.swift
+++ uApps-Support/uApps-Support/SwiftUI/FeedbackTableView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct FeedbackTableView: View {
+    @ObservedObject var viewModel = HomeViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            if viewModel.feedbackData.count == 0 {
+                Text("No Data")
+            } else {
+                ScrollView {
+                    ForEach(self.viewModel.feedbackData, id: \.self) { feedback in
+                        FeedbackCell(feedbackName: feedback.bugName, feedbackType: feedback.type, feedbackVersion: feedback.version, crash: feedback.didCrash).navigationTitle("Feedback").padding(.top, 5)
+                        Divider()
+                    }
+                }.padding(.top, 10)
+            }
+        }
+    }
+}
+
+
+struct FeedbackCell: View {
+    var feedbackName: String
+    var feedbackType: String
+    var feedbackVersion: String
+    var crash: Bool
+    
+    var body: some View {
+        VStack {
+            Text(feedbackName).font(.headline)
+            Divider()
+            Text("Type: \(feedbackType)")
+            HStack {
+                if crash {
+                    Text("Crash").foregroundColor(.red)
+                    Divider().frame(height: 20)
+                } else { EmptyView() }
+                Text(feedbackVersion)
+            }
+            Divider()
+        }
     }
 }
 
